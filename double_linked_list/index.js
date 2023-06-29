6f12ca99b186b9ebc08f3889c732cc47d96d573d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-06-29 11:19:54 +0900

    양방향 연결 리스트 shift 메서드 구현

--- double_linked_list/index.js
+++ double_linked_list/index.js
@@ -42,6 +42,23 @@ class DoubleLinkedList {
       return removeNode;
     }
   }
+  shift() {
+    if (this.length === 0) return undefined;
+
+    const oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+
+    this.length--;
+    return oldHead;
+  }
 }
 
 const doubleLinkedList = new DoubleLinkedList();
