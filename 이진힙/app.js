460a5b95e1ae43af5eb4e0ca71c50387a8623388
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-25 20:00:24 +0900

    우선 순위 큐(Queue) 솔루션

--- 이진힙/app.js
+++ 이진힙/app.js
@@ -71,15 +71,109 @@ class MaxBinaryHeap {
   }
 }
 
-const heap = new MaxBinaryHeap();
+// 우선순위 큐.
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
 
-heap.insert(41);
-heap.insert(39);
-heap.insert(33);
-heap.insert(18);
-heap.insert(27);
-heap.insert(12);
-heap.insert(55);
+  enqueue(value, priority) {
+    let newNode = new Node(value, priority);
 
-console.log(heap.values);
-console.log(heap.extractMax());
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+
+      if (element.priority >= parent.priority) break;
+
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+
+      idx = parentIdx;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      // sink down...
+      this.sinkDown();
+    }
+
+    return min;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swapIdx = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swapIdx = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swapIdx === null && rightChild.priority < element.priority) ||
+          (swapIdx !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swapIdx = rightChildIdx;
+        }
+      }
+
+      if (swapIdx === null) break;
+      this.values[idx] = this.values[swapIdx];
+      this.values[swapIdx] = element;
+      idx = swapIdx;
+    }
+  }
+}
+
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+//const heap = new MaxBinaryHeap();
+
+// heap.insert(41);
+// heap.insert(39);
+// heap.insert(33);
+// heap.insert(18);
+// heap.insert(27);
+// heap.insert(12);
+// heap.insert(55);
+
+//console.log(heap.values);
+//console.log(heap.extractMax());
+
+const priorityQueue = new PriorityQueue();
+
+priorityQueue.enqueue("코감기", 4);
+priorityQueue.enqueue("코로나", 1);
+priorityQueue.enqueue("독감", 3);
+priorityQueue.enqueue("비염", 5);
+priorityQueue.enqueue("코감기", 4);
