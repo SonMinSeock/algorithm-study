fb4ca59b16dfa1c5c06a8c30eea0d360451cc954
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-06-30 18:24:41 +0900

    해시 함수 성능 향상시키기

--- 해시테이블/app.js
+++ 해시테이블/app.js
@@ -1,11 +1,11 @@
 // 해시 함수 정의.
 function hash(str, size) {
   let total = 0;
-  for (const char of str) {
+  const WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(str.length, 100); i++) {
+    let char = str[i];
     let code = char.charCodeAt(0) - 96; // 해당 문자의 아스키코드 값.
-    total = (total + code) % size;
+    total = (total * WEIRD_PRIME + code) % size;
   }
   return total;
 }
-
-console.log(hash("pink", 10));
